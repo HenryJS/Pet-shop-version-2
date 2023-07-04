ed59fbb6c9c72fd825fbe3d17cbd46bb63cb9b7d
john henry <johnchenry9@gmail.com>
2023-07-04 21:58:20 +0300

    This is the inital setup

---
+++ src/components/nav/Navbar.js
@@ -0,0 +1,11 @@
+import './styles/Navbar.css';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="brand">
+                Petshop
+            </div>
+        </nav>
+    )
+}
