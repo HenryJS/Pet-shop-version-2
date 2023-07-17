6b6a17b6f495209f8f1add387e009cd0b0d3d82e
john henry <johnchenry9@gmail.com>
2023-07-17 09:24:41 +0300

    Fixing the Navbar error

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -1,13 +1,13 @@
 import Navbar from "../components/nav/Navbar";
-import Home from "../components/nav/home";
-import About from "../components/nav/about";
+
+
 
 export default function Homepage() {
     return (
       <div className="App">
         <Navbar />
-        <Home />
-        <About />
+      
+      
 
        
       </div>
