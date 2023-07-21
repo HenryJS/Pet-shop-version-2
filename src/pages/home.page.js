b7516b28a2fdf1071ae3a5a3b26c79913c516e81
john henry <johnchenry9@gmail.com>
2023-07-21 19:13:31 +0300

    addition of the breeds area at the homescreen to link to the products page.

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -1,10 +1,10 @@
-import Navbar from "../components/nav/Navbar";
+import Breeds from "../components/breeds/breeds"
 
 export default function Homepage() {
     return (
       <div className="App">
-        <Navbar />
-        <h1>One Stop Pet Shop React Project</h1>
+        <Breeds />
+        
       </div>
     )
 }
