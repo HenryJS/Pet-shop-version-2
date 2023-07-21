d5c5b3eea068da2c5dcf819dbf8294cc82937a20
john henry <johnchenry9@gmail.com>
2023-07-21 11:19:33 +0300

    addition of the pet details page, which shows details of the selected pet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/home.page'
+import Details from './pages/petdetails.page';
 
 
 function App(){
   return(
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Details />} />
       </Routes>
     </Router> 
   )
--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,11 +1,59 @@
+import React, { useState } from 'react';
+import Logo from "../assets/logo.jpg";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import CommentRoundedIcon from '@mui/icons-material/CommentRounded';
+import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+
+
 import './styles/Navbar.css';
 
-export default function Navbar() {
-    return (
-        <nav>
-            <div className="brand">
-                Petshop
-            </div>
-        </nav>
-    )
-}
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: 'Home',
+      icon: <HomeIcon />,
+    },
+    {
+      text: 'About',
+      icon: <InfoIcon />,
+    },
+    {
+      text: 'Testimonials',
+      icon: <CommentRoundedIcon />,
+    },
+    {
+      text: 'Contact',
+      icon: <PhoneRoundedIcon />,
+    },
+    {
+      text: 'Cart',
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
+
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt=''/>
+      </div>
+      <div className="navbar-links-container">
+        <a href="/">Home</a>
+        <a href="/">Available Breeds</a>
+        <a href="/">Testimonials</a>
+        <a href="/">About</a>
+       
+        <button className="primary-button">Login/sign-up</button>
+      </div>
+      <div className="navbar-menu-container">
+            <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+    </div>
+      
+    </nav>
+  );
+};
+
+export default Navbar;
