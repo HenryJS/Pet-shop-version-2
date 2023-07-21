6761828434b51d305c89c564aab888ddc0913d51
john henry <johnchenry9@gmail.com>
2023-07-21 13:43:36 +0300

    updating the navigation section

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import Logo from "../assets/logo.jpg";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import HomeIcon from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
+
 import CommentRoundedIcon from '@mui/icons-material/CommentRounded';
-import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
-import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+
+
 
 
 import './styles/Navbar.css';
@@ -17,22 +17,12 @@ const Navbar = () => {
       text: 'Home',
       icon: <HomeIcon />,
     },
-    {
-      text: 'About',
-      icon: <InfoIcon />,
-    },
+   
     {
       text: 'Testimonials',
       icon: <CommentRoundedIcon />,
     },
-    {
-      text: 'Contact',
-      icon: <PhoneRoundedIcon />,
-    },
-    {
-      text: 'Cart',
-      icon: <ShoppingCartRoundedIcon />,
-    },
+
   ];
 
   return (
@@ -44,8 +34,7 @@ const Navbar = () => {
         <a href="/">Home</a>
         <a href="/">Available Breeds</a>
         <a href="/">Testimonials</a>
-        <a href="/">About</a>
-       
+        
         <button className="primary-button">Login/sign-up</button>
       </div>
       <div className="navbar-menu-container">
