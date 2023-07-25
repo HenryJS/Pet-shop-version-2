d485fbf3f6307a265553f5a06012bf94d2b08dc8
john henry <johnchenry9@gmail.com>
2023-07-25 19:15:16 +0300

    Updating User Interface for smaller screens

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,48 +1,29 @@
 import React, { useState } from 'react';
-import Logo from "../assets/Logo.jpg";
-import { HiOutlineBars3 } from "react-icons/hi2";
-import HomeIcon from '@mui/icons-material/Home';
-import CommentRoundedIcon from '@mui/icons-material/CommentRounded';
-import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
-
-
-
+import Logo from "../assets/logo.jpg";
+import MenuIcon from '@mui/icons-material/Menu';
 import './styles/Navbar.css';
 
 const Navbar = () => {
-  const [openMenu, setOpenMenu] = useState(false);
-  const menuOptions = [
-    {
-      text: 'Home',
-      icon: <HomeIcon />,
-    },
-    {
-      text: 'Testimonials',
-      icon: <CommentRoundedIcon />,
-    },
-    {
-      text: 'Contact',
-      icon: <PhoneRoundedIcon />,
-    },
-  ];
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <nav>
       <div className="nav-logo-container">
         <img src={Logo} alt=''/>
       </div>
-      <div className="navbar-links-container">
+      <div className={`navbar-links-container ${isMobileMenuOpen ? "mobile-active" : ""}`}>
         <a href="/">Home</a>
         <a href="/">Available Breeds</a>
         <a href="/">Testimonials</a>
-        
-       
         <button className="primary-button">Login/sign-up</button>
       </div>
-      <div className="navbar-menu-container">
-            <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
-    </div>
-      
+      <div className="menu-icon-container">
+        <MenuIcon className="menu-icon" onClick={toggleMobileMenu} />
+      </div>
     </nav>
   );
 };
