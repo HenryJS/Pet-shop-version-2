30f06eb2f1e3a4a0d66efd4629721aecbbe404fb
john henry <johnchenry9@gmail.com>
2023-08-03 12:21:01 +0300

    Updates on the UI, and adding to cart options

--- src/components/products/CartContext.js
+++ src/components/products/CartContext.js
@@ -2,31 +2,71 @@ import React, { createContext, useContext, useState } from 'react';
 
 const CartContext = createContext();
 
-export function useCart() {
+export const useCart = () => {
   return useContext(CartContext);
-}
+};
 
-export function CartProvider({ children }) {
+export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
 
   const addToCart = (product) => {
-    const updatedCartItems = [...cartItems, product];
-    const newTotalPrice = totalPrice + product.price;
-    setCartItems(updatedCartItems);
-    setTotalPrice(newTotalPrice);
+    const existingCartItem = cartItems.find((item) => item.id === product.id);
+    if (existingCartItem) {
+      setCartItems((prevItems) =>
+        prevItems.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItems((prevItems) => [...prevItems, { ...product, quantity: 1 }]);
+    }
   };
 
-  const removeFromCart = (item) => {
-    const updatedCartItems = cartItems.filter((cartItem) => cartItem.id !== item.id);
-    const newTotalPrice = totalPrice - item.price;
-    setCartItems(updatedCartItems);
-    setTotalPrice(newTotalPrice);
+  const removeFromCart = (product) => {
+    setCartItems((prevItems) =>
+      prevItems.filter((item) => item.id !== product.id)
+    );
   };
+
+  const increaseQuantity = (product) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === product.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
+  };
+
+  const decreaseQuantity = (product) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === product.id && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+    );
+  };
+
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
 
   return (
-    <CartContext.Provider value={{ cartItems, totalPrice, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+        totalPrice,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
-}
+};
--- src/components/products/ourProducts.js
+++ src/components/products/ourProducts.js
@@ -8,11 +8,14 @@ import Horse1 from "../assets/horse1.jpg";
 import Footer from "../footer/footer";
 import Navbar from '../nav/Navbar';
 import { useCart } from '../products/cartcontext'; 
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import './style/products.css';
 
 const ProductsPage = () => {
-  const { cartItems, totalPrice, addToCart } = useCart(); 
+  const { cartItems, totalPrice, addToCart, increaseQuantity, decreaseQuantity, removeFromCart } = useCart(); 
 
   const products = [
     { id: 1, name: "Japanese Spitz", image: Dog1, price: 10000 },
@@ -20,8 +23,45 @@ const ProductsPage = () => {
     { id: 3, name: "Fluffy Cat", image: Cat1, price: 8000 },
     { id: 4, name: "German Shepherd 3", image: GS3, price: 20000 },
     { id: 5, name: "German Shepherd 4", image: GS4, price: 18000 },
-    { id: 6, name: "Horse ", image: Horse1, price: 50000 },
+    { id: 6, name: "Horse", image: Horse1, price: 50000 },
   ];
+
+  const isProductInCart = (productId) => {
+    return cartItems.some(item => item.id === productId);
+  };
+
+  const getProductQuantityInCart = (productId) => {
+    const cartItem = cartItems.find(item => item.id === productId);
+    return cartItem ? cartItem.quantity : 0;
+  };
+
+  const renderCartButtons = (product) => {
+    const productId = product.id;
+    const quantityInCart = getProductQuantityInCart(productId);
+    
+    if (isProductInCart(productId)) {
+      return (
+        <div>
+          <button className="secondary-button" onClick={() => decreaseQuantity(product)}>
+            <RemoveIcon />
+          </button>
+          <span>{quantityInCart}</span>
+          <button className="secondary-button" onClick={() => increaseQuantity(product)}>
+            <AddIcon />
+          </button>
+          <button className="remove-button" onClick={() => removeFromCart(product)}>
+            <DeleteIcon />
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <button className="secondary-button" onClick={() => addToCart(product)}>
+          Add to Cart
+        </button>
+      );
+    }
+  };
 
   return (
     <div>
@@ -35,8 +75,8 @@ const ProductsPage = () => {
           <div className="product" key={product.id}>
             <img src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
-            <p>Price: Ksh{product.price}</p>
-            <button className="secondary-button" onClick={() => addToCart(product)}>Add to Cart</button>
+            <p>Price: Ksh {product.price}</p>
+            {renderCartButtons(product)}
           </div>
         ))}
       </div>
