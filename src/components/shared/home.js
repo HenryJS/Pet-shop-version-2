ff40f3b98a0bd2401628fc4d2dfeb2409dd2d8b0
john henry <johnchenry9@gmail.com>
2023-08-07 10:58:18 +0300

    changing the home background

--- src/components/shared/home.js
+++ src/components/shared/home.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { FiArrowRight } from "react-icons/fi";
-import { Link } from "react-router-dom"; 
-import BannerBackground from "../assets/home-banner-background.png";
-//import BannerImage from "../assets/dogg.jpg";
+import { Link } from "react-router-dom";
+import BannerBackground from "../assets/home3.jpg";
 import "./styles/home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="home-banner-container">
-        <div className="home-bannerImage-container">
-          <img src={BannerBackground} alt="" />
-        </div>
+      <div className="home-banner-image-container" style={{ backgroundImage: `url(${BannerBackground})` }}></div>
         <div className="home-text-section">
           <h1 className="primary-heading">
             Your Journey of Owning a New Friend Starts Here!
@@ -26,9 +23,6 @@ const Home = () => {
               Adopt Now <FiArrowRight />
             </button>
           </Link>
-        </div>
-        <div className="home-image-container">
-          {/*<img src={BannerImage} alt="" />*/}
         </div>
       </div>
     </div>
