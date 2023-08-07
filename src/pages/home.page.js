1617537f89026efd082b9352e3d65e86a12adc36
john henry <johnchenry9@gmail.com>
2023-08-07 11:04:13 +0300

    further code cleanups

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -1,14 +1,10 @@
 
 import Home from "../components/shared/home";
 
-
-
 export default function Homepage() {
     return (
       <div className="App">
-        
         <Home />
-        
       </div>
     )
 }
