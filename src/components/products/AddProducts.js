e55d26e3e47a82403c9ff9a0e297b2d5edab96fc
john henry <johnchenry9@gmail.com>
2023-08-29 00:04:59 +0300

    Updating the products page view

--- src/components/products/AddProducts.js
+++ src/components/products/AddProducts.js
@@ -1,26 +1,30 @@
+import React from 'react';
 import Navbar from '../nav/Navbar';
 import { collection } from 'firebase/firestore';
 import { db } from '../../firebase';
-import { useCollectionData} from  "react-firebase-hooks/firestore"
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import SingleProductCard from './SingleProductCard';
-import Footer from '../footer/footer'
-
+import Footer from '../footer/footer';
+import './style/products.css'; // Import your CSS file
 
 const AddProducts = () => {
-  const [productsData] = useCollectionData(collection(db,"Products"));
-
-  console.log(productsData)
+  const [productsData] = useCollectionData(collection(db, 'Products'));
 
   return (
     <>
       <Navbar />
-      <div className=''> 
-        <h2 className="text-center font-semibold text-xl">PRODUCTS</h2>
-          <div className='product-container grid grid-cols-1 md:grid-cols-3 gap-4 justify-center items-center'>
-            {productsData?.map(product => (
-              <SingleProductCard key={product.ProductId} name={product.ProductName} price={product.ProductPrice} imageId={product.ProductImg} />
-            ))}
-         </div>
+      <div className=''>
+        <h2 className='text-center font-semibold text-xl'>PRODUCTS</h2>
+        <div className='product-container'>
+          {productsData?.map((product) => (
+            <SingleProductCard
+              key={product.ProductId}
+              name={product.ProductName}
+              price={product.ProductPrice}
+              imageId={product.ProductImg}
+            />
+          ))}
+        </div>
       </div>
       <Footer />
     </>
--- src/components/products/SingleProductCard.js
+++ src/components/products/SingleProductCard.js
@@ -1,61 +1,54 @@
+import React from "react";
 import { ref } from "firebase/storage";
 import { storage } from "../../firebase";
 import { useDownloadURL } from "react-firebase-hooks/storage";
 import { useCart } from "./cartcontext";
-import { useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
-import { AddCircle, RemoveCircle, Delete } from '@mui/icons-material';
-
+import { AddCircle, RemoveCircle, Delete } from "@mui/icons-material";
+import "./style/singleproduct.css"; // Import your CSS file
 
 export default function SingleProductCard({ name, price, imageId }) {
-    const navigate = useNavigate();
-    const { cartItems, addToCart, increaseQuantity, decreaseQuantity, removeFromCart } = useCart();
-    const user = auth.currentUser;
-    const [downloadUrl] = useDownloadURL(
-        ref(storage, `ProductImages/${imageId}.jpg`)
-     );
+  const navigate = useNavigate();
+  const { cartItems, addToCart, increaseQuantity, decreaseQuantity, removeFromCart } = useCart();
+  const user = auth.currentUser;
+  const [downloadUrl] = useDownloadURL(ref(storage, `ProductImages/${imageId}.jpg`));
 
-    const cartItem = cartItems.find(item => item.id === imageId);
+  const cartItem = cartItems.find((item) => item.id === imageId);
 
-    const handleAddToCart = () => {
-        if (user){
-            addToCart({ id: imageId, name, price });
-        } else {
-            navigate("/login");
-        }
+  const handleAddToCart = () => {
+    if (user) {
+      addToCart({ id: imageId, name, price });
+    } else {
+      navigate("/login");
     }
+  };
 
-    return (
-        <div className="w-full md:w-1/3 p-4 flex flex-col items-center">
-            <div className="max-w-xs mx-auto">
-                <img
-                    src={downloadUrl}
-                    className="object-contain h-56 w-56 mx-auto rounded-lg"
-                    alt="product"
-                />
-            </div>
-            <h3 className="text-lg font-semibold mt-2 text-center">{name}</h3>
-            <p className="text-gray-600 text-center">Price: Ksh {price}</p>
-            <button
-                className="mt-3 px-4 py-2 bg-orange-500 text-white rounded-full hover:bg-orange-400 transition duration-300 flex items-center"
-                onClick={(e) => {
-    e.stopPropagation(); // Prevent event from bubbling
-    handleAddToCart();
-  }}
-
-            >
-                {cartItem ? (
-                    <>
-                        <RemoveCircle onClick={() => decreaseQuantity(cartItem)} className="mr-1" />
-                        {cartItem.quantity}
-                        <AddCircle onClick={() => increaseQuantity(cartItem)} className="ml-1" />
-                        <Delete onClick={() => removeFromCart(cartItem)} className="ml-2" />
-                    </>
-                ) : (
-                    <>Add to Cart</>
-                )}
-            </button>
-        </div>
-    );
+  return (
+    <div className="product-card">
+      <div className="product-image-container">
+        <img src={downloadUrl} className="product-image" alt="product" />
+      </div>
+      <h3 className="product-name">{name}</h3>
+      <p className="product-price">Price: Ksh {price}</p>
+      <button
+        className="cart-button"
+        onClick={(e) => {
+          e.stopPropagation(); // Prevent event from bubbling
+          handleAddToCart();
+        }}
+      >
+        {cartItem ? (
+          <>
+            <RemoveCircle onClick={() => decreaseQuantity(cartItem)} className="cart-icon" />
+            {cartItem.quantity}
+            <AddCircle onClick={() => increaseQuantity(cartItem)} className="cart-icon" />
+            <Delete onClick={() => removeFromCart(cartItem)} className="cart-icon" />
+          </>
+        ) : (
+          <>Add to Cart</>
+        )}
+      </button>
+    </div>
+  );
 }
-
