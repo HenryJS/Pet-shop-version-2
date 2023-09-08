0e3cf9ddf58f848bd3d133596e2b5d8f19d67b1c
john henry <johnchenry9@gmail.com>
2023-09-08 15:04:49 +0300

    adding delete button

--- src/components/admin/allProducts.js
+++ src/components/admin/allProducts.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
+import { ref, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../firebase';
 import { db } from '../../firebase';
 import Nav from './adminnav';
 
-import './styles/allproducts.css'
-
+import './styles/allproducts.css';
 
 function AdminProducts() {
   const [products, setProducts] = useState([]);
@@ -27,42 +28,79 @@ function AdminProducts() {
     fetchProducts();
   }, []);
 
+  // Function to handle item deletion
+  const handleDeleteItem = async (productId) => {
+    try {
+      // Delete the item from Firestore based on its ID
+      await deleteDoc(doc(db, 'Products', productId));
+      // Remove the deleted item from the local state
+      setProducts((prevProducts) => prevProducts.filter((product) => product.id !== productId));
+    } catch (error) {
+      console.error('Error deleting product: ', error);
+    }
+  };
+
   return (
     <>
-    <Nav />
-    <h2 className="admin-products-heading">All Products</h2>
-    <div className="admin-products-container">
-      <table className="admin-products-table">
-        <thead>
-          <tr>
-            <th>Product ID</th>
-            <th>Product Name</th>
-            <th>Product Price</th>
-            <th>Product Image</th>
-            <th>Quantity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td>{product.ProductId}</td>
-              <td>{product.ProductName}</td>
-              <td>Ksh. {product.ProductPrice}</td>
-              <td>
-                <img
-                  src={product.ProductImg}
-                  alt={product.ProductName} 
-                  className="admin-product-image"
-                />
-              </td>
-              <td>{product.Quantity}</td>
+      <Nav />
+      <h2 className="admin-products-heading">All Products</h2>
+      <div className="admin-products-container">
+        <table className="admin-products-table">
+          <thead>
+            <tr>
+              <th>Product ID</th>
+              <th>Product Name</th>
+              <th>Product Price</th>
+              <th>Product Image</th>
+              <th>Delete</th> {/* Replace "Quantity" with "Delete" */}
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.ProductId}</td>
+                <td>{product.ProductName}</td>
+                <td>Ksh. {product.ProductPrice}</td>
+                <td>
+                  {/* Fetch and display image for each product */}
+                  <ProductImage imageId={product.ProductImg} />
+                </td>
+                <td>
+                  <button className="delete" onClick={() => handleDeleteItem(product.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-    
+  );
+}
+
+// Create a separate component to fetch and display product images
+function ProductImage({ imageId }) {
+  const [imageURL, setImageURL] = useState('');
+  const imageRef = ref(storage, `${imageId}`); 
+
+  useEffect(() => {
+    // Fetch the download URL when the component mounts
+    getDownloadURL(imageRef)
+      .then((downloadURL) => {
+        setImageURL(downloadURL);
+      })
+      .catch((error) => {
+        console.error('Error fetching download URL:', error);
+      });
+  }, [imageRef]);
+
+  return (
+    <div className="product-image">
+      {imageURL ? (
+        <img src={imageURL} className="product-image" alt="product" />
+      ) : (
+        <div>Loading image...</div>
+      )}
+    </div>
   );
 }
 
